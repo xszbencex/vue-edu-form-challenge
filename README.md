02ce418cc612affc8d2a736b1891470ca7189ed8
Varga György Márk <gyorgy.varga@shiwaforce.com>
2023-11-03 09:54:15 +0100

    init project

---
+++ README.md
@@ -0,0 +1,19 @@
+# Form challenge
+A feladat megoldásához szükséges kritériumok:
+
+- A form összes mezője legyen kötelező.
+- A form e-mail mezőjénél legyen megfelelő e-mail validáció.
+- A név nem lehet kevesebb 5 karakternél, illetve nem lehet nagyobb 250 karakternél
+- A jelszónak minimum 8 karakternek kell lennie és tartalmaznia kell (az egyszerűség kedvéért) minimum egy darab speciális karaktert (pl.: !.:)
+- A jelszó megerősítő input-ba beírt jelszónak meg kell egyeznie a felső jelszó mezővel
+- A "Sign up" gomb disabled legyen mindaddig, ameddig hibák vannak a form-ban
+- A "Sign up" gomb megnyomásánál csak simán írjuk ki az objektumot, amelyben a form elemei találhatóak
+
+
+To start this project you need to run:
+
+```npm install```
+
+and then
+
+```npm run dev```
---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <div
+    class="min-h-screen bg-gray-100 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
+  >
+    <div class="max-w-md w-full space-y-8">
+      <div>
+        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Sign up for an account
+        </h2>
+      </div>
+      <form class="mt-8 space-y-6" action="#" method="POST">
+        <div class="flex flex-col space-y-3 rounded-md shadow-sm -space-y-px">
+          <div>
+            <label
+              for="email-address"
+              class="block text-sm font-medium text-gray-700"
+              >Email address</label
+            >
+            <input
+              id="email-address"
+              name="email"
+              type="email"
+              autocomplete="email"
+              required
+              class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Email address"
+            />
+          </div>
+          <div>
+            <label for="name" class="block text-sm font-medium text-gray-700"
+              >Full name</label
+            >
+            <input
+              id="name"
+              name="name"
+              type="text"
+              autocomplete="name"
+              required
+              class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Full name"
+            />
+          </div>
+          <div>
+            <label
+              for="password"
+              class="block text-sm font-medium text-gray-700"
+              >Password</label
+            >
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autocomplete="new-password"
+              required
+              class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Password"
+            />
+          </div>
+          <div>
+            <label
+              for="password-again"
+              class="block text-sm font-medium text-gray-700"
+              >Password again</label
+            >
+            <input
+              id="password-again"
+              name="password-again"
+              type="password"
+              autocomplete="new-password"
+              required
+              class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Password again"
+            />
+          </div>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Sign Up
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
